2cf823fa565f6eee0d71082c6b88f461831c2b83
Arron Wilcock <Arron.wilcock@outlook.com>
2023-02-08 20:54:47 +0000

    dynamic urls created for multiple products, product page script created

--- front/js/script.js
+++ front/js/script.js
@@ -8,13 +8,16 @@ fetch("http://localhost:3000/api/products")
 
 const productCardHolder = document.getElementById("items");
 
+// This function collects data from the array of product in the api and inserts the into new cards on the homepage using dynamic HTML
+
 function insertProductCards(products) {
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
     const newProductCard = document.createElement("a");
     newProductCard.setAttribute("id", product._id);
-    // TODO find solution to href attribute below
-    newProductCard.setAttribute("href", "./product.html? + product._id");
+
+    const productUrl = `./product.html?id=${product._id}`;
+    newProductCard.setAttribute("href", productUrl);
 
     newProductCard.innerHTML = `
 <article>
