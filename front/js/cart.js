af2b4884f2f355a23f74eb6a3e512a7a4582d362
Arron Wilcock <Arron.wilcock@outlook.com>
2023-02-14 22:19:04 +0000

    cart item html added dynamically

---
+++ front/js/cart.js
@@ -0,0 +1,41 @@
+let cartPageItems = JSON.parse(localStorage.cartItems);
+
+console.log(cartPageItems);
+
+const cartItemDetails = document.getElementById("cart__items");
+
+populateCart(cartPageItems);
+
+function populateCart(cartPageItems) {
+  for (let i = 0; i < cartPageItems.length; i++) {
+    const cartItem = cartPageItems[i];
+    const newCartItem = document.createElement("article");
+    newCartItem.setAttribute("data-id", cartItem.id);
+    newCartItem.setAttribute("data-color", cartItem.color);
+    newCartItem.setAttribute("class", "class__item");
+
+    newCartItem.innerHTML = `
+<div class="cart__item__img">
+  <img src="../images/product01.jpg" alt="Photo of a sofa">
+</div>
+<div class="cart__item__content">
+  <div class="cart__item__content__description">
+    <h2>product name</h2>
+    <p>${cartItem.color}</p>
+    <p>€42.00</p>
+  </div>
+  <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+      <p>Quantity : </p>
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
+    </div>
+    <div class="cart__item__content__settings__delete">
+      <p class="deleteItem">Delete</p>
+    </div>
+  </div>
+</div>
+`;
+
+    cartItemDetails.appendChild(newCartItem);
+  }
+}
